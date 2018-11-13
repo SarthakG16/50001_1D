1ab6af088acef675ee644b0cc62b3d184a7d4e32
cmetramexs <32505600+cmetramexs@users.noreply.github.com>
2018-11-13 10:13:33 +0800

    no prog

--- SmartMirrorApp/app/src/main/java/com/example/sarth/smartmirrorapp/FilterActivity.java
+++ SmartMirrorApp/app/src/main/java/com/example/sarth/smartmirrorapp/FilterActivity.java
@@ -1,7 +1,30 @@
 package com.example.sarth.smartmirrorapp;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FilterActivity extends AppCompatActivity {
+    private Spinner spinner_filters;
+    //private ListView poster_list;
+    private ArrayAdapter filter_adapter;
 
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+
+        spinner_filters = findViewById(R.id.spinner_categories);
+        String[] filters = {"Select your Filter","Expiry Date","Location","Title"};
+
+        List<String> categories =  new ArrayList<>(Arrays.asList(filters));
+
+        filter_adapter = new ArrayAdapter(this,R.layout.list_i)
+    }
 }
