7117185ce825840fc0d1a3f93f35a2ff402f4599
cmetramexs <32505600+cmetramexs@users.noreply.github.com>
2018-11-24 13:49:22 +0800

    implemented intents

--- SmartMirrorApp/app/src/main/java/com/example/sarth/smartmirrorapp/LoginActivity.java
+++ SmartMirrorApp/app/src/main/java/com/example/sarth/smartmirrorapp/LoginActivity.java
@@ -5,12 +5,16 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity{
     private TextInputLayout login_username;
     private TextInputLayout login_password;
+
+    private String username;
+    private String password;
 
     private Button login_enter_button;
 
@@ -23,17 +27,59 @@ public class LoginActivity extends AppCompatActivity{
         login_enter_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HashMap<String, String> params = new HashMap<>();
-                Request req = new Request("POST","auth/login", params, new Request.Callback () {
-                    @Override
-                    public void onResponse(String response) {
-                        if (response.contains("success")){
-                            //Intent toHome = new Intent(LoginActivity.this,);
-                        }
+                boolean username_correct = validate(login_username);
+                boolean password_correct = validate(login_password);
+                if (!username_correct){
+                    Toast.makeText(LoginActivity.this,"Please enter your username.",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (!password_correct){
+                    Toast.makeText(LoginActivity.this,"Please enter your password.",Toast.LENGTH_LONG).show();
+                    return;
+                }
 
-                    }
-                });
+                username = login_username.getEditText().getText().toString().trim();
+                password = login_password.getEditText().getText().toString().trim();
+
+                HashMap<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                try{
+                    params.put("requested_privilege", "administrator");
+                    Request req = new Request("POST","auth/login", params, new Request.Callback () {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.contains("success")) {
+                                Intent toAdmin = new Intent(LoginActivity.this, AdminActivity.class);
+                                startActivity(toAdmin);
+                            }
+                        }
+                    });
+                } catch(Exception ex){
+                    params.put("requested_privilege","user");
+                    Request req = new Request("POST","auth/login", params, new Request.Callback () {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.contains("success")) {
+                                Intent toGuest = new Intent(LoginActivity.this, GuestActivity.class);
+                                startActivity(toGuest);
+                            }
+                        }
+                    });
+                }
             }
         });
+    }
+
+    private boolean validate(TextInputLayout check) {
+        String textInput = check.getEditText().getText().toString().trim();
+
+        if (textInput.isEmpty()) {
+            check.setError("Field can't be empty");
+            return false;
+        } else {
+            check.setError(null);
+            return true;
+        }
     }
 }
